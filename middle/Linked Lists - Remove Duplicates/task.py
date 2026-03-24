114ea5ade1d955b52a048d7a4ce36a97e6bd4089
Artahko <artem4iky8@gmail.com>
2026-03-24 22:00:13 +0200

    Complited Remove Duplicates task
    Implemented a function to remove duplicates from a linked list.

--- middle/Linked Lists - Remove Duplicates/task.py
+++ middle/Linked Lists - Remove Duplicates/task.py
@@ -1,4 +1,4 @@
-"""Task 1"""
+"""Task 8"""
 
 class Node:
     """Simple class representing a node in linked list"""
@@ -6,3 +6,22 @@ class Node:
     def __init__(self, data, next = None):
         self.data = data
         self.next = next
+
+def remove_duplicates(head):
+    if head is None:
+        return
+
+    prev_probe = head
+    probe = head.next
+
+    while probe is not None:
+        if prev_probe.data == probe.data:
+            prev_probe.next = probe.next
+            probe = probe.next
+
+            continue
+
+        prev_probe = probe
+        probe = probe.next
+
+    return head
