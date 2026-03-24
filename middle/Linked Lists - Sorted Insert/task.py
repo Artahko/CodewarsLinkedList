29a94e3ac42cbbf851bbf51a9849ac431c9a0b96
Artahko <artem4iky8@gmail.com>
2026-03-24 18:15:04 +0200

    initial commit

---
+++ middle/Linked Lists - Sorted Insert/task.py
@@ -0,0 +1,8 @@
+"""Task 1"""
+
+class Node:
+    """Simple class representing a node in linked list"""
+
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
