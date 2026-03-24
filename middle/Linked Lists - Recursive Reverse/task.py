b48af5d8cac84b16348ced8ef0a2018af39791c2
Artahko <artem4iky8@gmail.com>
2026-03-24 21:39:55 +0200

    Completed Remove Duplicates task

--- middle/Linked Lists - Recursive Reverse/task.py
+++ middle/Linked Lists - Recursive Reverse/task.py
@@ -1,4 +1,4 @@
-"""Task 1"""
+"""Task 7"""
 
 class Node:
     """Simple class representing a node in linked list"""
@@ -6,3 +6,29 @@ class Node:
     def __init__(self, data, next = None):
         self.data = data
         self.next = next
+
+    def __repr__(self):
+        return f"data: {self.data}"
+
+
+def reverse(head):
+    if head is None:
+        return head
+
+    def move_last_to_front(prev_node, node):
+        if node is None:
+            return
+
+        move_last_to_front(node, node.next)
+
+        temp_data = prev_node.data
+        prev_node.data = node.data
+        node.data = temp_data
+
+    probe = head
+
+    while probe.next is not None:
+        move_last_to_front(probe, probe.next)
+        probe = probe.next
+
+    return head
