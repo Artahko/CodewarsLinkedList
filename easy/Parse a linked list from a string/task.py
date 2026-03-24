3f97b08892b33cb0b3821617e08b768ecaace98e
Artahko <artem4iky8@gmail.com>
2026-03-24 18:41:42 +0200

    Completed Parse a linked list from a string task

--- easy/Parse a linked list from a string/task.py
+++ easy/Parse a linked list from a string/task.py
@@ -1,4 +1,4 @@
-"""Task 1"""
+"""Task 2"""
 
 class Node:
     """Simple class representing a node in linked list"""
@@ -6,3 +6,15 @@ class Node:
     def __init__(self, data, next = None):
         self.data = data
         self.next = next
+
+def linked_list_from_string(list_repr: str) -> Node | None:
+    if list_repr == "None":
+        return
+
+    linked_list = list_repr.split(" -> ")
+    nodes = [Node(int(el)) for el in linked_list[:-1]]
+
+    for i, node in enumerate(nodes[:-1]):
+        node.next = nodes[i+1]
+
+    return nodes[0]
