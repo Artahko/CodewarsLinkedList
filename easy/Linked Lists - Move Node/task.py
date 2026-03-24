c70e0f0c38a97ed3caf560fa94f14f60a2f3669f
Artahko <artem4iky8@gmail.com>
2026-03-24 19:14:41 +0200

    Completed Move node task

--- easy/Linked Lists - Move Node/task.py
+++ easy/Linked Lists - Move Node/task.py
@@ -1,4 +1,4 @@
-"""Task 1"""
+"""Task 5"""
 
 class Node:
     """Simple class representing a node in linked list"""
@@ -6,3 +6,21 @@ class Node:
     def __init__(self, data, next = None):
         self.data = data
         self.next = next
+
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+def move_node(source, dest):
+    if source is None:
+        raise ValueError("Can not move first node from an empty source linked list")
+
+    head_data = source.data
+    dest_head = dest
+
+    source = source.next
+    dest = Node(head_data)
+    dest.next = dest_head
+
+    return Context(source, dest)
