77babdeb31f58963c9b7cb3a5e6428021b82b8af
Artahko <artem4iky8@gmail.com>
2026-03-24 18:57:00 +0200

    Completed Push & build 123 task

--- easy/Linked Lists - Push & BuildOneTwoThree/task.py
+++ easy/Linked Lists - Push & BuildOneTwoThree/task.py
@@ -1,4 +1,4 @@
-"""Task 1"""
+"""Task 3"""
 
 class Node:
     """Simple class representing a node in linked list"""
@@ -6,3 +6,17 @@ class Node:
     def __init__(self, data, next = None):
         self.data = data
         self.next = next
+
+
+def push(head, data):
+    new_node = Node(data)
+    new_node.next = head
+
+    return new_node
+
+def build_one_two_three():
+    head = Node(3)
+    head = push(head, 2)
+    head = push(head, 1)
+
+    return head
