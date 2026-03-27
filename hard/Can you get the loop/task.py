8a4dea1c7e90f34ea97cf7e251a6861ed414692c
Artahko <artem4iky8@gmail.com>
2026-03-27 19:05:36 +0200

    Completed Detect the loop task

--- hard/Can you get the loop/task.py
+++ hard/Can you get the loop/task.py
@@ -6,3 +6,28 @@ class Node:
     def __init__(self, data, next = None):
         self.data = data
         self.next = next
+
+def loop_size(node):
+    slow = node.next
+    fast = node.next.next
+
+    # Look for a coliding point
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next.next
+
+    # Look for start of the loop
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next
+
+    starting_node = slow
+    count = 1
+    slow = slow.next
+
+    # Count the lenght of the loop
+    while starting_node != slow:
+        count += 1
+        slow = slow.next
+
+    return count
